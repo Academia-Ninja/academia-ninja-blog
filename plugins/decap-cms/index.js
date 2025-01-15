b8f9b81263c9129a3dab5f868267405a8c40ff6a
Ailton Loures <ailton.loures99@gmail.com>
2025-01-15 13:55:40 -0300

    feat: adicionando script para detectar nome do author

---
+++ plugins/decap-cms/index.js
@@ -0,0 +1,28 @@
+const user = fromUserData();
+
+function fromUserData() {
+    if (!window.netlifyIdentity) return;
+    const user = window.netlifyIdentity.currentUser();
+
+    return {
+        name: user.user_metadata.full_name,
+    }
+}
+
+function addUserDataIntoAnyField(user, field) {
+    const fieldLabel = Array.from(document.querySelectorAll('label').find(label => label.textContent.trim() === field));
+    const fieldInput = document.getElementById(fieldLabel.getAttribute('for'));
+
+    fieldInput.value = user.name;
+}
+
+function onPageChange() {
+    if (window.location.hash === '#/collections/blog/new')
+        addUserDataIntoAnyField(user, 'Author');
+}
+
+function registerWindowEvents() {
+    window.addEventListener('popstate', onPageChange);
+}
+
+registerWindowActions();
