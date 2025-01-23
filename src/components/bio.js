b1febdeff6d9c4bb122bfea44e38d36be89ff29c
Ailton Loures <ailton.loures99@gmail.com>
2025-01-23 11:18:05 -0300

    feat: atualizando texto de descrição

--- src/components/bio.js
+++ src/components/bio.js
@@ -25,16 +25,23 @@ const Bio = () => {
           <strong>
             <a href={author.site}>{author.name}</a>
           </strong>
-          , carioca, apaixonado por tecnologia e educação. Com mais de 6 anos de
-          experiência no setor, Ailton acumulou vivências enriquecedoras,
-          contribuindo tanto para projetos do mercado quanto para iniciativas no
-          mundo acadêmico e open source. Atualmente, atua como Engenheiro de
-          Software na TIVIT, consolidando sua expertise e impulsionando soluções
-          inovadoras no campo da tecnologia. <br />
-          <br />- Tá! mas...{" "}
+          , carioca, apaixonado por tecnologia e educação. Engenheiro de
+          Software com mais de 6 anos de experiência no setor, Ailton acumulou
+          vivências enriquecedoras, consolidando sua expertise e impulsionando
+          soluções inovadoras no campo da tecnologia. Ao longo da trajetória,
+          contribuiu (e ainda contribui!) tanto para projetos do mercado quanto
+          para iniciativas no mundo acadêmico e de código aberto.
+          <br />
+          <br />
+          - Beleza, mas... o que é a Academia Ninja?
+          <br />
+          <br />O objetivo da Academia Ninja sempre foi apoiar e instruir
+          calouros da faculdade e iniciantes na área de Tecnologia da
+          Informação, para todos poderem trocar ideias, contribuir em soluções e
+          em novos projetos. Conheça mais da nossa história em:{" "}
           <Link to="/academia-ninja/" itemProp="url">
             <strong>
-              <span itemProp="headline">o que é a Academia Ninja?</span>
+              <span itemProp="headline">O que é a Academia Ninja?</span>
             </strong>
           </Link>
         </p>
